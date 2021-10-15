f9c241434f88c345940a3a785e037c292d0d631b
Alejandro Mármol <amarmol20@gmail.com>
2021-10-15 09:50:18 -0600

    smartbrainapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation  from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value:60,
+      density:{
+        enable: true,
+        value_area: 700
+      }
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Particles className='particles'
+              params={particlesOptions}
+            />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () =>{
+    return(
+        <div>
+            <div className='gold f3 textshadow'>
+                {'Alejandro, your current rank is...'}
+            </div>
+            <div className='gold f1 textshadow'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
