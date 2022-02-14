175f51ba54e90dd2d7cb5f04a542103f87ffef24
Alejandro Renán Mármol Ruiz <amarmol@cnbs.gob.hn>
2022-02-14 10:57:25 -0600

    a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import { Component } from 'react';
 
 const particlesOptions = {
   particles: {
@@ -27,19 +28,31 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-       <Particles className='particles'
-              params={particlesOptions}
-            />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {input:'',}
+  }
+
+  onInputChange =(event) => {
+      console.log(event);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+                params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  };
+  
 }
 
 export default App;
